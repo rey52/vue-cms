2d8bd7df866ebb436054358172286dbc192d00d8
xiaogu <973466361@qq.com>
2019-10-14 23:23:12 +0800

    编写资讯列表

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '../components/tabbar/HomeContainer.vue'
-import member from '../components/tabbar/MemberContainer.vue'
-import shopcar from '../components/tabbar/ShopcarContainer.vue'
-import search from '../components/tabbar/SearchContainer.vue'
+import HomeContainer from '../components/tabbar/HomeContainer.vue'
+import MemberContainer from '../components/tabbar/MemberContainer.vue'
+import ShopcarContainer from '../components/tabbar/ShopcarContainer.vue'
+import SearchContainer from '../components/tabbar/SearchContainer.vue'
+import NewsList from '../components/news/NewsList.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/', redirect: '/home' },
-    { path: '/home', component: home },
-    { path: '/member', component: member },
-    { path: '/shopcar', component: shopcar },
-    { path: '/search', component: search }
+    { path: '/home', component: HomeContainer },
+    { path: '/member', component: MemberContainer },
+    { path: '/shopcar', component: ShopcarContainer },
+    { path: '/search', component: SearchContainer },
+    { path: '/home/newsList', component: NewsList }
   ],
   linkActiveClass: 'mui-active' // 覆盖默认路由高亮的类
 })
